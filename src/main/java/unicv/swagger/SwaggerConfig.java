75bf94ceaff8080a13517ce490649c09faaa17a5
Marco da Costa <marco.costa@student.unicv.edu.cv>
2023-07-12 12:17:40 -0100

    feat: configure swagger ui

---
+++ src/main/java/unicv/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package unicv.swagger;
+
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import unicv.utils.Constants;
+
+@OpenAPIDefinition(
+        tags = {
+                @Tag(name = Constants.SWAGGER_TAG_NAME,
+                description = Constants.SWAGGER_TAG_DESCRIPTION)
+        },
+        info = @Info(
+                title = Constants.SWAGGER_INFO_TITLE,
+                description = Constants.SWAGGER_INFO_DESCRIPTION,
+                version = Constants.SWAGGER_INFO_VERSION,
+                contact = @Contact(
+                        name = Constants.SWAGGER_INFO_CONTACT_NAME,
+                        url = Constants.SWAGGER_INFO_CONTACT_URL,
+                        email = Constants.SWAGGER_INFO_CONTACT_URL
+                ),
+                license = @License(
+                        name = Constants.SWAGGER_LICENCE_NAME,
+                        url = Constants.SWAGGER_LICENCE_URL
+                )
+        )
+)
+public class SwaggerConfig extends Application {
+}
--- src/main/java/unicv/utils/Constants.java
+++ src/main/java/unicv/utils/Constants.java
@@ -3,4 +3,12 @@ package unicv.utils;
 public class Constants {
     public static final String SWAGGER_TAG_NAME = "";
     public static final String SWAGGER_TAG_DESCRIPTION = "";
+    public static final String SWAGGER_INFO_TITLE = "";
+    public static final String SWAGGER_INFO_DESCRIPTION = "";
+    public static final String SWAGGER_INFO_VERSION = "";
+    public static final String SWAGGER_INFO_CONTACT_NAME = "";
+    public static final String SWAGGER_INFO_CONTACT_URL = "";
+    public static final String SWAGGER_INFO_CONTACT_EMAIL = "";
+    public static final String SWAGGER_LICENCE_NAME = "";
+    public static final String SWAGGER_LICENCE_URL = "";
 }
