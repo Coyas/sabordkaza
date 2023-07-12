1b8d989beea8a0c6c64d12432dbc4a96e0928425
Ailton M. Duarte <adidas.coyas@gmail.com>
2023-07-12 15:26:45 -0100

    feat: create swagger config

---
+++ src/main/java/cv/sabordkasa/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package cv.sabordkasa.swagger;
+
+import cv.sabordkasa.constants.Constants;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import jakarta.ws.rs.core.Application;
+
+@OpenAPIDefinition(
+        tags = {
+                @Tag(name = "teste de swagger", description = "etste de descriptokn")
+        },
+        info = @Info(
+                title = "Servico Sabor D'Kaza  - API",
+                description = "e-Service - lista de receitas",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = " Support sabordkaza",
+                        url = "https://sabordkaza.cv",
+                        email = "support@sabordkaza.cv"
+                ),
+                license = @License(
+                        name = "Licença CLoud",
+                        url = "https://sabordkaza.cv"
+                )
+        )
+)
+public class SwaggerConfig extends Application {
+}
