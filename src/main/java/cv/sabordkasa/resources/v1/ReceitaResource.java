94142246abf663e7948a75c383b6732d07d1b083
Ailton M. Duarte <adidas.coyas@gmail.com>
2023-07-12 16:21:01 -0100

    fix: comentarios e db pool

--- src/main/java/cv/sabordkasa/resources/v1/ReceitaResource.java
+++ src/main/java/cv/sabordkasa/resources/v1/ReceitaResource.java
@@ -3,6 +3,7 @@ package cv.sabordkasa.resources.v1;
 import cv.sabordkasa.constants.Constants;
 import cv.sabordkasa.models.DataBean;
 import cv.sabordkasa.models.PayloadResponse;
+import cv.sabordkasa.models.Receitas;
 import cv.sabordkasa.repositories.ReceitaRepository;
 import cv.sabordkasa.utils.Utils;
 import jakarta.inject.Inject;
@@ -12,6 +13,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.transaction.Transactional;
 
 
 @Path("/v1")
@@ -24,16 +26,25 @@ public class ReceitaResource {
 
 
     @GET
-    @Tag(name = "end point")
-    @Operation(summary = "summary", description = "description")
+    @Tag(name = "Lista todos as receitas")
+    @Operation(summary = "lista der citeeas registradas", description = "vai listar todas as receitas ate agora")
     @Path("/receitas")
-    public PayloadResponse getStudents(){
+    public PayloadResponse getReceitas(){
         return Utils.payloadResponse(
                 DataBean
                         .builder()
                         .code(0)
                         .payload(receitaRepository.findAllReceitas())
                         .build());
+    }
+
+    @POST
+    @Tag(name = "Permite criar uma receita")
+    @Operation(summary = "envia os dados sobre a receita para o bd", description = "envia dados por um formulario para o banco de dados psql")
+    @Path("/receitas")
+    @Transactional
+    public PayloadResponse insertReceitas(Receitas receitas) {
+        return receitaRepository.save(receitas);
     }
 
 }
